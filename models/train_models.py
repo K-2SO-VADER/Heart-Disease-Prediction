26b72aeff0e3be395c256fef5ffff343598a4ee2
zack <zacmwangi60@gmail.com>
2017-10-27 20:59:07 +0300

    Added: Random Forest Classifier

--- models/train_models.py
+++ models/train_models.py
@@ -5,9 +5,11 @@ from models.read_csv import split_data
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
+from models.read_csv import prediction_variables
+from sklearn.ensemble import RandomForestClassifier
 
 path = '/home/zack/Desktop/ML/AI_CLASS/Data/reprocessedHungarianData'
-data = split_data(path)
+data = split_data(path, prediction_variables)
 
 train_x = data[0]
 train_y = data[1]
@@ -23,9 +25,18 @@ def decision_tree_model():
     print('Accuracy Score: ', accuracy_score(prediction, test_y))
 
 
+# fit a Random Classifier
+def predict_rf_model():
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(train_x, train_y)
+    prediction = model.predict(test_x)
+    rf_accuracy = accuracy_score(prediction, test_y)
+    return rf_accuracy
+
+
 # use grid search CV to find the parameters for prediction using decision trees
 def model_grid_search_cv(model, param_grid, data_x, data_y):
-    clf = GridSearchCV(model, param_grid, cv= 5, scoring="accuracy")
+    clf = GridSearchCV(model, param_grid, cv=5, scoring="accuracy")
     clf.fit(train_x, train_y)
     print("Best Parameters: ")
     print(clf.best_params_)
@@ -58,8 +69,15 @@ decision_tree_model()
 print("----GridSearchCV----")
 decision_trees_grid_search()
 
+print("----- Random Forest Classifier-----")
+print(predict_rf_model())
+
 '''
 Accuracy Score:  0.584269662921
+
+----- Random Forest Classifier------
+0.707865168539 (Without Parameter Tuning)
+
 
 ----GridSearchCV----
 Best Parameters: 
@@ -71,9 +89,9 @@ Best Accuracy Score:
 '''
     To do:
     1. Play around with parameters to improve accuracy
-    2. Try out other algorithms
-    3. Hold out set to reduce overfitting.
-    4. Find the best prediction variables.  
+    2. Hold out set to reduce overfitting.
+    3. Find the best prediction variables from all >76 features/prediction variables
+      
 '''
 
 
